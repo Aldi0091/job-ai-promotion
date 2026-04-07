1ba4a646f8875eb8443ef714fe46eada576f61ea
aldo91 <akhanovkz@gmail.com>
2026-04-08 00:19:28 +0500

    Update API url

--- src/utils/http.ts
+++ src/utils/http.ts
@@ -1,4 +1,4 @@
-const API_URL = (import.meta as any)?.env?.VITE_API_URL ?? "http://localhost:8079";
+const API_URL = import.meta.env.VITE_API_URL ?? "http://localhost:8079";
 
 
 export const httpJson = async (path: string, init?: RequestInit) => {
