0eb3abf2ed968bc0f927ee46013d17c3864bbabf
aldo91 <akhanovkz@gmail.com>
2026-04-13 21:16:33 +0500

    Fix Job List UI/UX Grid

--- src/App.tsx
+++ src/App.tsx
@@ -7,25 +7,30 @@ import { upworkJobsResource, upworkFavoritesResource } from "./resources/upworkJ
 import { UpworkSemanticSearch } from "./resources/upworkJobs/semanticSearch";
 import { UpworkAsk } from "./resources/upworkJobs/ask";
 import StarIcon from "@mui/icons-material/Star";
-import WorkOutlineIcon from "@mui/icons-material/WorkOutline";
+import TableChartIcon from "@mui/icons-material/TableChart";
+import ListAltIcon from "@mui/icons-material/ListAlt";
+import ManageSearchIcon from "@mui/icons-material/ManageSearch";
+import AutoAwesomeIcon from "@mui/icons-material/AutoAwesome";
 import { jobApplicationsResource } from "./resources/jobApplications";
 
 export default function App() {
   return (
     <LayoutProvider>
       <Admin dataProvider={dataProvider} layout={MyLayout}>
-        <Resource {...upworkJobsResource} />
-        <Resource {...jobApplicationsResource} icon={WorkOutlineIcon} />
+        <Resource {...upworkJobsResource} icon={TableChartIcon} />
+        <Resource {...jobApplicationsResource} icon={ListAltIcon} />
         <Resource {...upworkFavoritesResource} icon={StarIcon} />
         <Resource
           name="upwork-jobs-search"
           list={UpworkSemanticSearch}
           options={{ label: "Semantic Search" }}
+          icon={ManageSearchIcon}
         />
         <Resource
           name="upwork-jobs-ask"
           list={UpworkAsk}
           options={{ label: "Ask AI" }}
+          icon={AutoAwesomeIcon}
         />
       </Admin>
     </LayoutProvider>
